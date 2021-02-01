ed8545189efddabf4b2180f938727340ad29c059
Akshay <akshaysharma@gmail.com>
2021-02-01 11:42:05 +0530

    first

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer() {
+    return (
+        <div>
+            <h1>number of cake</h1>
+            <button>Buy Cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer
